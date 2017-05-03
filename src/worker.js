c62668efc0f707cb7c2c16877ea83ea7754671e3
Kevin Peno <kpeno@deloitte.com>
2017-05-03 15:28:37 -0700

    updated docker compose to run eddn as a separate process; added heroku stuff

---
+++ src/worker.js
@@ -0,0 +1,52 @@
+/* global require, console */
+"use strict"
+// EDDN listener
+const memcache = require("memory-cache")
+const zmq = require("zeromq")
+const sock = zmq.socket("sub")
+const eddn = require("./libs/eddn")
+const _ = require("lodash")
+
+sock.connect("tcp://eddn-relay.elite-markets.net:9500")
+sock.subscribe("")
+console.log("Worker connected to port 9500")
+
+function updateIfNeeded(a) {
+	return a.map((faction) => {
+		const old = memcache.get(faction.name)
+		const toSave = Object.assign({}, old, faction)
+
+		if (!_.isEqual(old, toSave)) {
+			return !!memcache.put(faction.name, toSave)
+		}
+		else {
+			return false
+		}
+	}).some(v => v === true)
+}
+
+function processMessage(message) {
+	return eddn.getMessage(message)
+		.then((topic) => {
+			const updated = updateIfNeeded(eddn.getFactionsFromLocation(topic))
+			const factions = topic.message.Factions.sort((a, b) => {
+				return b.Influence - a.Influence
+			})
+			const factionsText = factions.reduce((acc, faction) => {
+				const influencePct = Math.floor(faction.Influence * 100)
+				return acc + `${faction.Name}: ${influencePct}%\n`
+			}, "")
+
+			const logTxt = `${topic.message.event} event for "${topic.message.StarSystem}"
+Factions:
+
+${factionsText}
+Updated: ${updated.toString()}
+
+`
+			console.log(logTxt)
+		})
+		.catch(() => {})
+}
+
+sock.on("message", processMessage)
